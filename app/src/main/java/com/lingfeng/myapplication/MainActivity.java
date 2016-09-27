6012bae92825d7c0e89990990f2f769da6fa3d8a
975109629ljm <l>
2016-09-27 20:37:21 +0800

    修改打印值

--- app/src/main/java/com/lingfeng/myapplication/MainActivity.java
+++ app/src/main/java/com/lingfeng/myapplication/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.i("tag","456");
+        Log.i("tag","789");
     }
 }
